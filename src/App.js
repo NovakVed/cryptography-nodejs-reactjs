d699b2614598139dd4ad5f5ddcbbc3df56e33234
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2020-11-21 23:26:03 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import {Route, BrowserRouter as Router, Link} from 'react-router-dom';
-import './App.css';
+import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+import Navbar from './Components/inc/Navbar'
+import EncryptComponents from './Components/CenteredGrid'
 import About from './Pages/About'
 import Home from './Pages/Home'
 
@@ -8,15 +14,22 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-        </nav>
+        <React.Fragment>
+          <CssBaseline />
+          <Navbar />
+          <EncryptComponents />
+          <Container>
+            <nav>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+              </ul>
+            </nav>
 
-        <Route path="/about" component={About} />
-        <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/" exact component={Home} />
+          </Container>
+        </React.Fragment>
       </Router>
     );
   }
